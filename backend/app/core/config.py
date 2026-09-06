d0253862bdd549af25bbb733b4f5b972a12f2e99
NirmalPatel-02 <nilupatel02005@gmail.com>
2026-09-06 06:56:55 +0530

    auth update for token

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = Field(min_length=32)
     JWT_ALGORITHM: str = "HS256"
 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 240
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     OTP_EXPIRE_MINUTES: int = 10
